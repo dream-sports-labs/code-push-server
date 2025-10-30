d8620553031d96b081c9c62e91453a4c524112fb
Devansh Saini <devansh.saini@dream11.com>
2025-10-30 23:20:02 +0530

    fix: added otel packages

--- api/script/instrumentation.ts
+++ api/script/instrumentation.ts
@@ -3,12 +3,21 @@ import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentation
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-proto';
 import { PeriodicExportingMetricReader } from '@opentelemetry/sdk-metrics';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { MySQL2Instrumentation } from '@opentelemetry/instrumentation-mysql2';
+import { resourceFromAttributes } from '@opentelemetry/resources';
 
 const sdk = new opentelemetry.NodeSDK({
+  serviceName: 'code-push-server',
+  resource: resourceFromAttributes({
+    'service.name': 'code-push-server',
+    'service.component': 'code-push-server',
+  }),
   traceExporter: new OTLPTraceExporter(),
   metricReader: new PeriodicExportingMetricReader({
     exporter: new OTLPMetricExporter(),
   }),
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [getNodeAutoInstrumentations(), new HttpInstrumentation(), new ExpressInstrumentation(), new MySQL2Instrumentation()],
 });
 sdk.start();
